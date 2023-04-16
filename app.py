2932e045a66d257a50bd5d2563b027bb3fbcdc74
Rafael Souza Pinto <rafa.souza.pinto@gmail.com>
2023-04-15 22:11:52 -0300

    Fix hello name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - GabyDias"
+    return "Hello World - Rafa"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
